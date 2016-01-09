60d1b731422e3cf7388231848494bb34e9579d24
garyleetw <gary.lee.tw@gmail.com>
2016-01-09 11:05:33 +0800

    1st commit

---
+++ views/api/post.js
@@ -0,0 +1,29 @@
+'use strict';
+
+exports.readPost = function(req, res, next) {
+};
+
+exports.createPost = function(req, res, next) {
+  var title = req.query.title;
+  var message = req.query.message;
+};
+
+exports.readPostById = function(req, res, next) {
+
+};
+
+exports.readPostBySubject = function(req, res, next) {
+
+};
+
+exports.updatePost = function(req, res, next) {
+
+}
+
+exports.publish = function(req, res, next) {
+
+}
+
+exports.unpublish = function(req, res, next) {
+
+}
